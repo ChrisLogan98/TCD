f1f0a48f974179572faa0db1008fad5e39e6d688
Christian Logan <55796957+ChrisLogan98@users.noreply.github.com>
2021-02-10 11:28:53 -0600

    Add files via upload

---
+++ Connect 4 against computer/Connect 4/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Connect_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.turnBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nEWSINGLEPLAYERGAMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nEWMULTIPLAYERGAMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.turnBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem,
+            this.hELPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(525, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nEWSINGLEPLAYERGAMEToolStripMenuItem,
+            this.nEWMULTIPLAYERGAMEToolStripMenuItem,
+            this.eXITToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
+            this.eXITToolStripMenuItem.Text = "E&XIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBOUTToolStripMenuItem});
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hELPToolStripMenuItem.Text = "HELP";
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aBOUTToolStripMenuItem.Text = "ABOUT";
+            this.aBOUTToolStripMenuItem.Click += new System.EventHandler(this.aBOUTToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Connect_4.Properties.Resources.blank_board;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 305);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(111, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(160, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(42, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(209, 70);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(44, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "E";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(260, 70);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(44, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "F";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(311, 70);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(39, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "G";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // turnBox
+            // 
+            this.turnBox.Image = global::Connect_4.Properties.Resources.turn_red;
+            this.turnBox.Location = new System.Drawing.Point(389, 99);
+            this.turnBox.Name = "turnBox";
+            this.turnBox.Size = new System.Drawing.Size(50, 50);
+            this.turnBox.TabIndex = 9;
+            this.turnBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Turn";
+            // 
+            // nEWSINGLEPLAYERGAMEToolStripMenuItem
+            // 
+            this.nEWSINGLEPLAYERGAMEToolStripMenuItem.Name = "nEWSINGLEPLAYERGAMEToolStripMenuItem";
+            this.nEWSINGLEPLAYERGAMEToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
+            this.nEWSINGLEPLAYERGAMEToolStripMenuItem.Text = "NEW SINGLEPLAYER GAME";
+            // 
+            // nEWMULTIPLAYERGAMEToolStripMenuItem
+            // 
+            this.nEWMULTIPLAYERGAMEToolStripMenuItem.Name = "nEWMULTIPLAYERGAMEToolStripMenuItem";
+            this.nEWMULTIPLAYERGAMEToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
+            this.nEWMULTIPLAYERGAMEToolStripMenuItem.Text = "NEW MULTIPLAYER GAME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 416);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.turnBox);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Connect 4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.turnBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox turnBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem nEWSINGLEPLAYERGAMEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nEWMULTIPLAYERGAMEToolStripMenuItem;
+    }
+}
+
---
+++ Connect 4 against computer/Connect 4/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Connect_4
+{
+    public partial class Form1 : Form
+    {
+        //possible square values
+        const int EMPTY = 0;
+        const int RED = 1;
+        const int BLACK = 2;
+
+        //board dimensions
+        const int WIDTH = 7;
+        const int HEIGHT = 6;
+        const int TILE_SIZE = 50;
+        //turn storage
+        int whoseTurn = RED;
+
+        int[,] board = new int[WIDTH, HEIGHT]; //board is 7x6
+
+        Graphics graphics; //draw
+
+        SoundPlayer checkerDropSound = new SoundPlayer(global::Connect_4.Properties.Resources.snd_checker_drop);
+
+        Image redSquare = global::Connect_4.Properties.Resources.square_red;
+        Image blackSquare = global::Connect_4.Properties.Resources.square_black;
+        Image emptySquare = global::Connect_4.Properties.Resources.square_empty;
+        Image turnBlack = global::Connect_4.Properties.Resources.turn_black;
+        Image turnRed = global::Connect_4.Properties.Resources.turn_red;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pictureBox1.CreateGraphics();
+            Reset();
+        }
+
+        private void aBOUTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("BEHOLD! I BRING YOU CONNECT 4 IN C#.");
+        }
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void Reset()
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    board[i, j] = EMPTY;
+                }
+            }
+        }
+        void DrawBoard()
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    if(board[i, j] == EMPTY)
+                    {
+                        graphics.DrawImage(emptySquare, i * TILE_SIZE, j * TILE_SIZE);
+                    }
+                    else if (board[i, j] == BLACK)
+                    {
+                        graphics.DrawImage(blackSquare, i * TILE_SIZE, j * TILE_SIZE);
+                    }
+                    else if (board[i, j] == RED)
+                    {
+                        graphics.DrawImage(redSquare, i * TILE_SIZE, j * TILE_SIZE);
+                    }
+                }
+            }
+        }
+
+        void DropChecker(int col)
+        {
+            for (int i = HEIGHT - 1; i >= 0; i--)
+            {
+                if (board[col, i] == EMPTY)
+                {
+                    board[col, i] = whoseTurn;
+                    DrawBoard();
+                    checkerDropSound.Play();
+                    HandleWinningMove();
+                    SwitchTurn();
+                    break;
+                }
+            }
+        }
+
+        void SwitchTurn()
+        {
+            if(whoseTurn == RED)
+            {
+                whoseTurn = BLACK;
+                turnBox.Image = global::Connect_4.Properties.Resources.turn_black;
+            }
+            else
+            {
+                turnBox.Image = global::Connect_4.Properties.Resources.turn_red;
+                whoseTurn = RED;
+            }
+        }
+
+        //returns horizontal connect 4 anywhere on
+        bool HorizontalConnect4()
+        {
+            for (int col = 0; col <= 3; col++)
+            {
+                for (int row = 0; row < HEIGHT; row++)
+                {
+                    //cannot start on empty square
+                    if (board[col, row] != EMPTY)
+                    {
+                        if (board[col, row] == board[col + 1, row] &&
+                            board[col, row] == board[col + 2, row] &&
+                            board[col, row] == board[col + 3, row])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        bool VerticalConnect4()
+        {
+            for (int col = 0; col < WIDTH; col++)
+            {
+                for (int row = 0; row <= 2; row++)
+                {
+                    //cannot start on empty square
+                    if (board[col, row] != EMPTY)
+                    {
+                        if (board[col, row] == board[col, row+1] &&
+                            board[col, row] == board[col, row+2] &&
+                            board[col, row] == board[col, row+3])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        bool DiagonalDownConnect4()
+        {
+            for (int col = 0; col <+ 3; col++)
+            {
+                for (int row = 0; row <= 2; row++)
+                {
+                    //cannot start on empty square
+                    if (board[col, row] != EMPTY)
+                    {
+                        if (board[col, row] == board[col - 1, row + 1] &&
+                            board[col, row] == board[col - 2, row + 2] &&
+                            board[col, row] == board[col - 3, row + 3])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        bool DiagonalUpConnect4()
+        {
+            for (int col = 0; col <= 3; col++)
+            {
+                for (int row = HEIGHT  - 3; row < HEIGHT; row++)
+                {
+                    //cannot start on empty square
+                    if (board[col, row] != EMPTY)
+                    {
+                        if (board[col, row] == board[col + 1, row - 1] &&
+                            board[col, row] == board[col + 2, row - 2] &&
+                            board[col, row] == board[col + 3, row - 3])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        void HandleWinningMove()
+        {
+            if(HorizontalConnect4() || VerticalConnect4() || DiagonalUpConnect4() || DiagonalDownConnect4())
+            {
+                DrawBoard();
+                MessageBox.Show("Connect 4!");
+                Reset();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DropChecker(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DropChecker(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DropChecker(2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DropChecker(3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DropChecker(4);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DropChecker(5);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DropChecker(6);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DropChecker(7);
+        }
+
+        private void nEWGAMEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private bool TryChecker(int row, int color)
+        {
+            for (int i = HEIGHT - 1; i >= 0; i--)
+            { 
+                
+            }
+            return false;
+        }
+
+        private void RemoveChecker(int col)
+        {
+            for (int i = 0; i >= HEIGHT; i++)
+            {
+                
+            }
+        }
+
+
+    }
+}
